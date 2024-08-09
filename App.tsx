149017e24fd97d35a8ed799f5436e0d38ad78cd2
gokhanyanik <gokhanyanik@gmail.com>
2024-08-09 12:42:59 +0300

    loglama

--- App.tsx
+++ App.tsx
@@ -57,7 +57,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
+ console.log("app started")
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
