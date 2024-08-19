f234d76c4865c52ddbf5983e8049b77328dede16
gokhanyanik <gokhanyanik@gmail.com>
2024-08-19 14:26:07 +0300

    Image Komponenti

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
 } from 'react-native';
 
 import {
@@ -32,11 +33,20 @@ function App(): React.JSX.Element {
   return (
     /* İlk textte bir özellik ve style verme anlatıldı
     ikinci texte text içinde metod kullanma anlatıldı */
-    <SafeAreaView style={{ flex: 1, backgroundColor: "red", justifyContent: "center", alignItems: "center" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "red" }}>
       <Text numberOfLines={2} style={{ color: "white", fontWeight: "bold" }}>
         lorem ipsun dolar set amet lorem ipsun dolar set amet lorem ipsun dolar set amet lorem ipsun dolar set amet lorem ipsun dolar set amet
       </Text>
-      <Text onPress={() => console.log("event gerçekleşti...")}>hello mydarling</Text>
+      <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyRceKo4bqjJuw_Rp2dHHqwlyuL0IgDTah8nz55ys7_K1CzLacEtmjBq-VoU6c35y21pM&usqp=CAU",
+          width: 300,
+          height: 300
+        }}
+        resizeMode='contain'   //..... internet üzerinden yüklenen resimlerde boyutlandırma yapılır.
+      //blurRadius={10}
+      //Eğer localde bir resim yüklenicekse uri:"" yerine require("./hangi dizinde ise") kullanılır.
+      />
     </SafeAreaView>
   );
 }
