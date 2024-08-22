c74c0069faca0f4e1c14eb483a467d139dea669e
gokhanyanik <gokhanyanik@gmail.com>
2024-08-22 16:55:44 +0300

    Flexbox ve FlexDirection Kullanımı

--- App.tsx
+++ App.tsx
@@ -8,23 +8,23 @@ import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
-  Platform,
+  View
 } from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.bacground}>
-     
+     <View style={{width:100,height:75,backgroundColor:"purple"}}/>
+     <View style={{width:100,height:75,backgroundColor:"yellow"}}/>
+     <View style={{width:100,height:75,backgroundColor:"orange"}}/>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   bacground: {
-    backgroundColor: Platform.OS == "ios" ? "yellow" : "gray",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: "brown",
+    flex: 1,  // flex viewlerin boyutunu belirler ve 1 değerini alması alanın tamamını kaplar.
+    flexDirection:"row" // elementleri sağdan sola dizer.
   }
 });
 export default App;
