fdc29516eea660037952e2f66ad556d61c93e1ae
gokhanyanik <gokhanyanik@gmail.com>
2024-08-22 13:07:46 +0300

    Card Uygulaması

--- App.tsx
+++ App.tsx
@@ -11,89 +11,84 @@ import {
   StyleSheet,
   Text,
   Platform,
-  Dimensions
+  Dimensions,
+  Image,
+  FlatList,
+  View
 
 
 } from 'react-native';
 const data = [
   {
-    "userId": 1,
+    "albumId": 1,
     "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "https://via.placeholder.com/600/392537",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
   },
   {
-    "userId": 1,
+    "albumId": 1,
     "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    "title": "reprehenderit est deserunt velit ipsam",
+    "url": "https://via.placeholder.com/600/56acb2",
+    "thumbnailUrl": "https://via.placeholder.com/150/771796"
   },
   {
-    "userId": 1,
+    "albumId": 1,
     "id": 3,
-    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    "title": "officia porro iure quia iusto qui ipsa ut modi",
+    "url": "https://via.placeholder.com/600/51aa97",
+    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
   },
   {
-    "userId": 1,
+    "albumId": 1,
     "id": 4,
-    "title": "eum et est occaecati",
-    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    "url": "https://via.placeholder.com/600/35185e",
+    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
   },
   {
-    "userId": 1,
+    "albumId": 1,
     "id": 5,
-    "title": "nesciunt quas odio",
-    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-  },
-  {
-    "userId": 1,
-    "id": 6,
-    "title": "dolorem eum magni eos aperiam quia",
-    "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
-  },
-  {
-    "userId": 1,
-    "id": 7,
-    "title": "magnam facilis autem",
-    "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
-  },
-  {
-    "userId": 1,
-    "id": 8,
-    "title": "dolorem dolore est ipsam",
-    "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "nesciunt iure omnis dolorem tempora et accusantium",
-    "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
-  },
-  {
-    "userId": 1,
-    "id": 10,
-    "title": "optio molestias id quia eum",
-    "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+    "title": "natus nisi omnis corporis facere molestiae rerum in",
+    "url": "https://via.placeholder.com/600/121fa4",
+    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
   },
 ]
 function App(): React.JSX.Element {
-  console.log(Platform.OS, Dimensions.get("screen"))
+
   return (
     <SafeAreaView style={styles.bacground}>
-      <Text style={[styles.text, styles.title]}>This is an {Platform.OS} App</Text>
-      <Text>Height:{Dimensions.get("screen").height}</Text>
-      <Text>Width:{Dimensions.get("screen").width}</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) =>
+          <View style={{ flexDirection: "row" }}>
+            <View>
+              <Image
+                source={{
+                  uri: item.url,
+                  width: 200,
+                  height: 200,
+                }}
+                borderRadius={10}
+              />
+            </View>
+            <View style={styles.viewText}>
+              <Text style={[styles.title]}>{item.title}</Text>
+              <Text style={styles.text}>Id: {item.id}</Text>
+            </View>
+          </View>
+        }
+      />
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   bacground: {
-    backgroundColor: Platform.OS == "ios" ? "yellow" : "green",
+    backgroundColor: Platform.OS == "ios" ? "yellow" : "gray",
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-
   },
   text: {
     color: "black",
@@ -101,9 +96,13 @@ const styles = StyleSheet.create({
   },
   title: {
     color: "white",
-    fontWeight: "bold"
+    fontWeight: "bold",
+    margin: 5
+  },
+  viewText: {
+    backgroundColor: "orange",
+    width: 200,
+    borderRadius:10
   }
-
 });
-
 export default App;
