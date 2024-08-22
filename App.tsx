90032b704115ef0b687811160952c481f397e7ce
gokhanyanik <gokhanyanik@gmail.com>
2024-08-22 17:18:19 +0300

    JustifyContent ve alignItems Kullanımı

--- App.tsx
+++ App.tsx
@@ -14,9 +14,10 @@ import {
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.bacground}>
-     <View style={{width:100,height:75,backgroundColor:"purple"}}/>
-     <View style={{width:100,height:75,backgroundColor:"yellow"}}/>
-     <View style={{width:100,height:75,backgroundColor:"orange"}}/>
+      <View style={{ width: 100, flexGrow:1,height: 75, backgroundColor: "purple" }} />
+      <View style={{ width: 100, height: 75, backgroundColor: "yellow" }} />
+      <View style={{ width: 100, height: 75, backgroundColor: "orange" }} />
+      
     </SafeAreaView>
   );
 }
@@ -24,7 +25,11 @@ const styles = StyleSheet.create({
   bacground: {
     backgroundColor: "brown",
     flex: 1,  // flex viewlerin boyutunu belirler ve 1 değerini alması alanın tamamını kaplar.
-    flexDirection:"row" // elementleri sağdan sola dizer.
+    flexDirection: "row", // elementleri sağdan sola dizer.
+    justifyContent: "center",
+    alignItems: "center",
+    //flexWrap: "wrap",// yana dizilen elementlerden sığmama durumlarında kullanılır ve sığmayanları alt satıra alır.
+   // alignContent: "center"
   }
 });
 export default App;
