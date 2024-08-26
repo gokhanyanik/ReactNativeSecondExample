e1cce0d7061d2577d319c7e14d9d184cee43ba70
gokhanyanik <gokhanyanik@gmail.com>
2024-08-26 11:58:56 +0300

    React Hooks Kullanımı

--- App.tsx
+++ App.tsx
@@ -4,36 +4,51 @@
  *
  * @format
  */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   View,
-  Text
+  Text,
+  Button
 } from 'react-native';
-const ColeredView=(props:any)=>{
-  return(
-    <View style={{ flex:1, backgroundColor:props.arkaPlan }}>
-      <Text>{props.text}</Text>
-    </View>
-  )
-}
+
+
+
 function App(): React.JSX.Element {
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+  useEffect(() => {
+    console.log("Her ubdate edildiğinde çalişicak", count)
+  }) //Her ubdate edildiğinde çalişir.
+  useEffect(() => {
+    console.log("sadece ilk rander edildiğinde çalişicak", count)
+  }, [])//ilk render edildiğinde çalışır
+  useEffect(() => {
+    console.log("Sadece count değeri ubdate edildiğinde çalişir", count)
+  }, [count])//Her count useEffect'i update ecildiğinde çalişir.Yani farklı bir useEffet ubdate edildiğinde çalişmayacak.Count değerine bağli olarak çalişir
   return (
-    // propsları yazarken parantez kullanmak şart değildir.Parantez olmadan da string ifade yazılabilir.
     <SafeAreaView style={styles.bacground}>
-      <ColeredView arkaPlan={"purple"} text="ali"   />  
-      <ColeredView arkaPlan="yellow" text="veli"  />
-      <ColeredView arkaPlan="orange" text="mehmet"/>
-      
+      <Text style={{ color: "white" }}>Count: {count}</Text>
+      <Button
+        title="up"
+        onPress={() => setCount(count + 1)}
+      />
+       <Text style={{ color: "white" }}>Count2: {count2}</Text>
+      <Button
+        title="up"
+        onPress={() => setCount2(count2 + 1)}
+      />
+
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   bacground: {
     backgroundColor: "brown",
-    flex: 1,  
-    
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 export default App;
