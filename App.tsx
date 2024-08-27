8a1af743a91f7d377794a33c5c5bd42680c781cc
gokhanyanik <gokhanyanik@gmail.com>
2024-08-27 15:00:55 +0300

    Api Call İşlemleri

--- App.tsx
+++ App.tsx
@@ -16,30 +16,14 @@ import {
 
 
 function App(): React.JSX.Element {
-  const [count, setCount] = useState(0)
-  const [count2, setCount2] = useState(0)
-  useEffect(() => {
-    console.log("Her ubdate edildiğinde çalişicak", count)
-  }) //Her ubdate edildiğinde çalişir.
-  useEffect(() => {
-    console.log("sadece ilk rander edildiğinde çalişicak", count)
-  }, [])//ilk render edildiğinde çalışır
-  useEffect(() => {
-    console.log("Sadece count değeri ubdate edildiğinde çalişir", count)
-  }, [count])//Her count useEffect'i update ecildiğinde çalişir.Yani farklı bir useEffet ubdate edildiğinde çalişmayacak.Count değerine bağli olarak çalişir
-  return (
-    <SafeAreaView style={styles.bacground}>
-      <Text style={{ color: "white" }}>Count: {count}</Text>
-      <Button
-        title="up"
-        onPress={() => setCount(count + 1)}
-      />
-       <Text style={{ color: "white" }}>Count2: {count2}</Text>
-      <Button
-        title="up"
-        onPress={() => setCount2(count2 + 1)}
-      />
-
+  const fetchData=()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+  };
+    return (
+      <SafeAreaView style={styles.bacground}>
+      <Button title="press me" onPress={fetchData}/>
     </SafeAreaView>
   );
 }
